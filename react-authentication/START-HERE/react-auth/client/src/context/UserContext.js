8c9f3f8008158c3bfcc789691a285be547bb2924
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-13 13:10:51 -0800

    preserve the user state with cookies

--- react-authentication/START-HERE/react-auth/client/src/context/UserContext.js
+++ react-authentication/START-HERE/react-auth/client/src/context/UserContext.js
@@ -1,9 +1,12 @@
 import { createContext, useState } from "react";
+import Cookies from "js-cookie";
 
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
-  const [authUser, setAuthUser] = useState(null);
+  const cookie = Cookies.get('authenticatedUser');
+  const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
+
 
   const signIn = async (credentials) => {
     // encoding user credentials following the basic authentication scheme requirement
@@ -24,6 +27,8 @@ export const UserProvider = (props) => {
     if (response.status === 200){
       const user = await response.json();
       setAuthUser(user);
+      // storing cookie name: "authenticatedUser", value of user stringidied, expires in 1 day
+      Cookies.set("authenticatedUser", JSON.stringify(user), {expires: 1});
       return user
     } else if (response.status === 401){
       return null
@@ -35,6 +40,7 @@ export const UserProvider = (props) => {
 
   const signOut = () => {
     setAuthUser(null);
+    Cookies.remove('authenticatedUser');
   }
 
   return (
