07b8e54211db9a45d84e865806f6af4f1ddea68c
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-13 10:09:16 -0800

    set up user registration

--- react-authentication/START-HERE/react-auth/client/src/components/UserSignUp.js
+++ react-authentication/START-HERE/react-auth/client/src/components/UserSignUp.js
@@ -1,9 +1,10 @@
 import { useContext, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ThemeContext from '../context/ThemeContext';
 
 const UserSignIn = () => {
   const { accentColor } = useContext(ThemeContext);
+  const navigate = useNavigate();
 
   // State
   const name = useRef(null);
@@ -12,12 +13,54 @@ const UserSignIn = () => {
   const [errors, setErrors] = useState([]);
 
   // event handlers
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
+    const user = {
+      name: name.current.value,
+      username: username.current.value,
+      password: password.current.value 
+    };
+
+    // Defining options to pass to the fectch method
+    const fetchOptions = {
+      // To add new user, method is POST
+      method: "POST",
+      // In case HTTP server needs it, we're defining the body type via a content type request header...
+      headers: {
+        // which lets the server know the body type is json with character encoding utf-8
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      // send user's info in the body property and ensure it's a string object with JSON.stringify()
+      body: JSON.stringify(user),
+    };
+
+    // to catch errors when using async/await, we need a try/catch block
+    try {
+      const response = await fetch("http://localhost:5000/api/users", fetchOptions);
+      if (response.status === 201){
+        console.log(`${user.username} is successfully signed up and authenticated!`);
+      } else if (response.status === 400){
+        // if not successful, we need to parse the results. We need to do that with response.json(), which returns a promise.
+        // Add await to the front to wait for the promise to be fulfilled
+        const data = await response.json();
+        // console.log(data)
+        setErrors(data.errors)
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      // If there's an error, log the error and navigate to the error route
+      console.log(error);
+      // need React Router's useNavigate() hook to naviagate to a new route
+      navigate("/error");
+    }
   }
 
   const handleCancel = (event) => {
     event.preventDefault();
+    // navigate back to the root if the user clicks 'cancel'
+    navigate('/')
   }
 
   return (
